f6c6c194b8557743bf6e8796a9035831d708b646
massimo.daros <massimo.daros@nttdata.com>
2023-08-15 11:42:07 +0200

    Updated lesson011 exercises

--- src/main/scala/mdaros/exercises/scala/chapter2/lesson011/Lesson001.scala
+++ src/main/scala/mdaros/exercises/scala/chapter2/lesson011/Lesson001.scala
@@ -5,7 +5,9 @@ object Lesson001 extends App {
   val carloCollodi = new Writer ( "Carlo", "Collodi", 1826 )
 
   val pinocchio = new Novel ( "Pinocchio", 1883, carloCollodi )
+  val pinocchioRel2 = pinocchio.copy ( 1885 )
 
   println ( s"${pinocchio.name} was written by ${carloCollodi.fullName ()}? ${pinocchio.isWrittenBy ( carloCollodi )}" )
   println ( s"${carloCollodi.fullName ()} age when he written ${pinocchio.name}: ${pinocchio.authorAge()}" )
+  println ( s"${carloCollodi.fullName ()} age when he written ${pinocchioRel2.name} (second release): ${pinocchioRel2.authorAge()}" )
 }
